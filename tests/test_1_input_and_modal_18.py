0e676d5d9e4c89fa3dff631dbc2d3a5a245d2106
EAK <anastasiya.er2@mail.ru>
2023-05-04 16:42:46 +0300

    git push

--- tests/test_1_input_and_modal_18.py
+++ tests/test_1_input_and_modal_18.py
@@ -2,6 +2,7 @@ import pickle
 import time
 import pytest
 from selenium import webdriver
+from webdriver_manager import drivers
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.basket_page_3 import Basket_page
 from pages.catalog_page_2 import Catalog_page
@@ -10,38 +11,38 @@ from pages.login_page_1 import Login_page
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
+service = Service('path/to/chromedriver')
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
 def test_login_page():
-    # options = webdriver.ChromeOptions()
-    # options.add_argument("start-maximized")
-    # options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    #s = Service('C:\\Users\\anast\\chromedriver.exe')
+
     driver = webdriver.Chrome()
-    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    # login = Login_page(driver)
-    # login.autorization_without_cookie() #авторизация для сохранения кук
-    # pickle.dump(driver.get_cookies(), open("cookies_01.pkl", "wb"))
-    # print("куки сохранены")
-    # time.sleep(5)
-
+    # авторизация для сохранения кук
     login = Login_page(driver)
-    login.autorization_with_cookie() #авторизация с уже сохраненными куками
-    for cookie in pickle.load(open("cookies_01.pkl", "rb")):
-        driver.add_cookie(cookie)
+    login.autorization_without_cookie()
+    pickle.dump(driver.get_cookies(), open("cookies_01.pkl", "wb"))
+    print("куки сохранены")
     time.sleep(5)
-    # driver.get("https://www.coolclever.ru/")
-    # time.sleep(7)
 
+    # авторизация с уже сохраненными куками
+    # login = Login_page(driver)
+    # login.autorization_with_cookie()
+    # for cookie in pickle.load(open("cookies_01.pkl", "rb")):
+    #     driver.add_cookie(cookie)
+    # time.sleep(5)
+    # drivers.get("https://www.coolclever.ru/")
+    # time.sleep(7)
 
     cp = Catalog_page(driver)
     cp.catalog()
 
-    # bp = Basket_page(driver)
-    # bp.empty_basket()
+    bp = Basket_page(driver)
+    bp.empty_basket()
 
     ip = Items_page(driver)
     ip.products()
-
+    driver.quit()
 
 
 
